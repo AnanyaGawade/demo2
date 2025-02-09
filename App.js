004f1aa6b8bde2dc46ef464ad831633d58ddea57
Ananya Gawade <149894215+AnanyaGawade@users.noreply.github.com>
2025-02-09 22:22:24 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PuneAgriMarket from "./PuneAgriMarket";
+
+function App() {
+  return (
+    <div>
+      <PuneAgriMarket />
+    </div>
+  );
+}
+
+export default App;
